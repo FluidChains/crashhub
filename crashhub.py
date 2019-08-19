4cce4d51f28bce896b6a3389178c1aa7c414b2a7
Karim Boucher <boucherkarim@gmail.com>
2019-08-19 18:23:16 -0400

    Update crashhub to use blueprints

--- crashhub.py
+++ crashhub.py
@@ -1,29 +1,10 @@
 #!/bin/python3
-import click
 
-from lib.routes import app
-from lib import util
+from flask import Flask
+from lib.routes import bp
 
-
-@click.group()
-def cli():
-    pass
-
-
-@cli.command()
-def run():
-    app.run(host="0.0.0.0", port=8000)
-
-
-@click.command()
-@click.option('--no-dry-run', is_flag=True, help='Don\'t actually change anything.')
-def update_posts(no_dry_run):
-    if not no_dry_run:
-        print("Dry-run, changes will not be performed.")
-    util.update_posts(not no_dry_run)
-
-
-cli.add_command(update_posts)
+app = Flask(__name__)
+app.register_blueprint(bp)
 
 if __name__ == "__main__":
-    cli()
+    app.run(host="0.0.0.0", port=8000)
