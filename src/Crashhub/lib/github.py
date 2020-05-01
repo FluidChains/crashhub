cac3bfbe01a1696fce16392620a69693e0611714
Karim Boucher <boucherkarim@gmail.com>
2020-05-01 10:23:21 -0400

    Remove print statements

--- src/Crashhub/lib/github.py
+++ src/Crashhub/lib/github.py
@@ -16,38 +16,29 @@ def get_github():
     return g.github
 
 def report_issue(title, body):
-    print("report issue")
     repo = get_github()
     issue = repo.create_issue(title, body)
     return issue.number
 
 
 def update_issue(id, body):
-    print("update issue")
     repo = get_github()
     repo.get_issue(id).edit(body=body)
     return id
 
 
 def issue_is_closed(id):
-    print("issue is closed")
     repo = get_github()
     closed_by = repo.get_issue(id).closed_by
     return closed_by is not NotSet and closed_by is not None and hasattr(closed_by, "login")
 
 
 def issue_closed_by(id):
-    print("issue closed by")
     repo = get_github()
     return repo.get_issue(id).closed_by
 
 
 def respond(id, body):
-    print("respond")
     repo = get_github()
-    try:
-        print(body)
-        repo.get_issue(id).create_comment(body)
-    except Exception as e:
-        print('create_comment')
-        print(e)
+    repo.get_issue(id).create_comment(body)
+
